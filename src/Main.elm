3ae998ea553ab2390b4231250c8f28b2867b8b13
natsumisawa <natsumi.sawa@bizreach.co.jp>
2019-12-07 15:37:21 +0900

    🎉

--- src/Main.elm
+++ src/Main.elm
@@ -1,12 +1,12 @@
 module Main exposing (main)
 
 import Browser
-import Element exposing (Length, alignLeft, alpha, centerX, column, el, fill, fillPortion, image, layout, maximum, padding, px, rgba255, row, spacing, text, width)
+import Element exposing (Length, alignLeft, alpha, centerX, column, el, fill, fillPortion, image, layout, maximum, padding, px, rgba255, row, spacing, text, width, wrappedRow)
 import Element.Background exposing (color)
 import Element.Border exposing (rounded)
 import Element.Events exposing (onClick)
 import Element.Font exposing (color, size)
-import Element.Input exposing (labelHidden, placeholder, text)
+import Element.Input exposing (button, labelHidden, placeholder, text)
 import Html exposing (Html)
 import Html.Attributes
 import Html.Events
@@ -20,13 +20,21 @@ import Html.Events
 
 type alias Model =
     { name : String
-    , clickedIndexList : List Int
+    , selectedLR : LeftOrRight
+    , clickedIndexListLeft : List Int
+    , clickedIndexListRight : List Int
+    , isFinishedChoice : Bool
     }
+
+
+type LeftOrRight
+    = Left
+    | Right
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( { name = "", clickedIndexList = [] }, Cmd.none )
+    ( { name = "", selectedLR = Left, clickedIndexListLeft = [], clickedIndexListRight = [], isFinishedChoice = False }, Cmd.none )
 
 
 
@@ -37,13 +45,16 @@ init _ =
 
 type Msg
     = InputName String
+    | OnClickLeftButton
+    | OnClickRightButton
     | OnClickButton Int
+    | GenerateSeal
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     let
-        { name, clickedIndexList } =
+        { name, selectedLR, clickedIndexListLeft, clickedIndexListRight } =
             model
     in
     case msg of
@@ -51,13 +62,54 @@ update msg model =
             ( { model | name = n }, Cmd.none )
 
         OnClickButton index ->
-            ( { model
-                | clickedIndexList =
-                    if clickedIndexList |> List.member index then
-                        clickedIndexList |> List.filter (\i -> i /= index)
+            case selectedLR of
+                Left ->
+                    ( { model
+                        | clickedIndexListLeft =
+                            if clickedIndexListLeft |> List.member index then
+                                clickedIndexListLeft |> List.filter (\i -> i /= index)
 
-                    else
-                        index :: clickedIndexList
+                            else if List.length clickedIndexListLeft >= 4 then
+                                clickedIndexListLeft
+
+                            else
+                                index :: clickedIndexListLeft
+                      }
+                    , Cmd.none
+                    )
+
+                Right ->
+                    ( { model
+                        | clickedIndexListRight =
+                            if clickedIndexListRight |> List.member index then
+                                clickedIndexListRight |> List.filter (\i -> i /= index)
+
+                            else if List.length clickedIndexListRight >= 4 then
+                                clickedIndexListRight
+
+                            else
+                                index :: clickedIndexListRight
+                      }
+                    , Cmd.none
+                    )
+
+        GenerateSeal ->
+            ( { model
+                | isFinishedChoice = True
+              }
+            , Cmd.none
+            )
+
+        OnClickLeftButton ->
+            ( { model
+                | selectedLR = Left
+              }
+            , Cmd.none
+            )
+
+        OnClickRightButton ->
+            ( { model
+                | selectedLR = Right
               }
             , Cmd.none
             )
@@ -72,35 +124,125 @@ update msg model =
 view : Model -> Html Msg
 view model =
     let
-        { name, clickedIndexList } =
+        { name, selectedLR, clickedIndexListLeft, clickedIndexListRight, isFinishedChoice } =
             model
     in
     layout [ Element.Background.color (rgba255 223 107 160 100) ] <|
         column [ padding 30, spacing 30, centerX ]
-            [ el [ color (rgba255 255 255 255 100), centerX, size 40 ] (Element.text "おなまえシールめーかー")
-            , Element.Input.text
-                [ width (px 400), centerX ]
-                { onChange = \n -> InputName n
-                , text = name
-                , placeholder = Just <| placeholder [] (Element.text "おなまえ")
-                , label = labelHidden "name"
-                }
-            , buttonsView 1 clickedIndexList
-            , buttonsView 7 clickedIndexList
-            , buttonsView 14 clickedIndexList
+            [ el [ size 40, color white ] (Element.text "おなまえシールめーかー")
+            , row [ spacing 5, centerX, width (px 470) ]
+                [ button
+                    [ case selectedLR of
+                        Left ->
+                            Element.Background.color
+                                (rgba255 103 182 166 100)
+
+                        Right ->
+                            Element.Background.color
+                                (rgba255 220 220 220 100)
+                    , rounded 50
+                    , padding 10
+                    , centerX
+                    , color white
+                    ]
+                    { onPress = Just OnClickLeftButton
+                    , label = Element.text "←"
+                    }
+                , Element.Input.text []
+                    { onChange = \n -> InputName n
+                    , text = name
+                    , placeholder = Just <| placeholder [] (Element.text "おなまえ")
+                    , label = labelHidden "name"
+                    }
+                , button
+                    [ case selectedLR of
+                        Left ->
+                            Element.Background.color
+                                (rgba255 220 220 220 100)
+
+                        Right ->
+                            Element.Background.color
+                                (rgba255 103 182 166 100)
+                    , rounded 50
+                    , padding 10
+                    , centerX
+                    , color white
+                    ]
+                    { onPress = Just OnClickRightButton
+                    , label = Element.text "→"
+                    }
+                ]
+            , if (List.length clickedIndexListLeft >= 4) || (List.length clickedIndexListRight >= 84) then
+                el [ size 12, color white, centerX ] <| Element.text "それぞれ4つまでしか選べません！"
+
+              else
+                Element.none
+            , case selectedLR of
+                Left ->
+                    buttonsView clickedIndexListLeft
+
+                Right ->
+                    buttonsView clickedIndexListRight
+
+            -- , button
+            --     [ Element.Background.color (rgba255 103 182 166 100)
+            --     , rounded 50
+            --     , padding 10
+            --     , centerX
+            --     , color white
+            --     ]
+            --     { onPress = Just GenerateSeal
+            --     , label = Element.text "つくる"
+            --     }
+            , -- if isFinishedChoice then
+              row [ Element.Background.color white ] <|
+                List.append
+                    ((Element.text name
+                        :: List.map
+                            (\i ->
+                                el [] <|
+                                    image
+                                        [ width <| px 50 ]
+                                        { src = "assets/" ++ String.fromInt i ++ ".PNG"
+                                        , description = ""
+                                        }
+                            )
+                            clickedIndexListLeft
+                     )
+                        |> List.reverse
+                    )
+                <|
+                    List.map
+                        (\i ->
+                            el [] <|
+                                image
+                                    [ width <| px 50 ]
+                                    { src = "assets/" ++ String.fromInt i ++ ".PNG"
+                                    , description = ""
+                                    }
+                        )
+                        clickedIndexListRight
+
+            -- else
+            --   Element.none
             ]
 
 
-buttonsView : Int -> List Int -> Element.Element Msg
-buttonsView startIndex clickedIndexList =
-    row [ spacing 5 ] <|
+white : Element.Color
+white =
+    rgba255 255 255 255 100
+
+
+buttonsView : List Int -> Element.Element Msg
+buttonsView clickedIndexList =
+    wrappedRow [ spacing 5, width (px 470) ] <|
         List.map
             (\i ->
                 el
-                    [ onClick <| OnClickButton i
-                    , rounded 50
+                    [ rounded 50
                     , padding 5
-                    , Element.Background.color (rgba255 255 255 255 100)
+                    , Element.Background.color white
+                    , onClick <| OnClickButton i
                     ]
                 <|
                     image
@@ -115,7 +257,7 @@ buttonsView startIndex clickedIndexList =
                         , description = ""
                         }
             )
-            (List.range startIndex (startIndex + 7))
+            (List.range 1 21)
 
 
 main : Program () Model Msg
